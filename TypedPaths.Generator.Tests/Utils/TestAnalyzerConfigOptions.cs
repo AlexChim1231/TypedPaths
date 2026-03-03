4426d7d4fbc49bce0ae388f8b50239f1621af8ef
AlexChimTrm <132866042+AlexChim1231@users.noreply.github.com>
2026-03-03 14:08:50 +0800

    fix warning

--- TypedPaths.Generator.Tests/Utils/TestAnalyzerConfigOptions.cs
+++ TypedPaths.Generator.Tests/Utils/TestAnalyzerConfigOptions.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 
 using Microsoft.CodeAnalysis.Diagnostics;
 
@@ -7,22 +8,19 @@ namespace TypedPaths.Generator.Tests.Utils;
 /// <summary>
 /// Test double for <see cref="AnalyzerConfigOptions"/> that returns config from a dictionary.
 /// </summary>
-public sealed class TestAnalyzerConfigOptions : AnalyzerConfigOptions
+public sealed class TestAnalyzerConfigOptions(IReadOnlyDictionary<string, string>? values = null)
+    : AnalyzerConfigOptions
 {
-    private readonly IReadOnlyDictionary<string, string> _values;
+    private readonly IReadOnlyDictionary<string, string> _values = values ?? new Dictionary<string, string>();
 
-    public TestAnalyzerConfigOptions(IReadOnlyDictionary<string, string>? values = null)
-    {
-        _values = values ?? new Dictionary<string, string>();
-    }
-
-    public override bool TryGetValue(string key, out string? value)
+    public override bool TryGetValue(string key, [NotNullWhen(true)] out string? value)
     {
         if (_values.TryGetValue(key, out var v))
         {
             value = v;
             return true;
         }
+
         value = null;
         return false;
     }
